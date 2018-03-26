424111c6f77e5c1a63ffc2936263e8d9f58802fc
Bryantle <bryantl@berkeley.edu>
2018-03-26 12:40:53 -0700

    UIAttached Behavior, UIDynamic Animator, UIGravity

--- iOS Experiment 4 - UIKit Dynamics Attach Behavior/ViewController.swift
+++ iOS Experiment 4 - UIKit Dynamics Attach Behavior/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var anchorPoint: CGPoint!
+    var attachment: UIAttachmentBehavior!
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    
+    @IBOutlet weak var smallView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        attachment.anchorPoint = sender.location(in: view)
+        smallView.center = sender.location(in: view)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 1
+        anchorPoint = smallView.center
+        
+        // 2
+        attachment = UIAttachmentBehavior(item: redView, attachedToAnchor: anchorPoint)
+        attachment.length = 100
+        attachment.frequency = 10
+        attachment.damping = 5
+        
+        // 3
+        animator = UIDynamicAnimator(referenceView: view)
+        animator.addBehavior(attachment)
+        
+        // 4
+        gravity = UIGravityBehavior(items: [redView])
+        animator.addBehavior(gravity)
     }
 
     override func didReceiveMemoryWarning() {
